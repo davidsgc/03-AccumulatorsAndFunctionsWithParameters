ad5445afe35e17e9d97da06a311f8b07bdbc9760
davidsgc <davidsgc@rose-hulman.edu>
2019-01-26 21:08:17 -0500

    circles

--- src/m8_loops_and_objects.py
+++ src/m8_loops_and_objects.py
@@ -6,8 +6,8 @@ This module demonstrates simple LOOPS of the form:
 and also USING OBJECTS.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Gabby Davidson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -15,7 +15,8 @@ import rosegraphics as rg
 def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
-
+    print_sequence1()
+    draw_circles1()
 
 def print_sequence1():
     """
@@ -29,7 +30,7 @@ def print_sequence1():
        200
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its doc-string above.
+    # DONE: 2. Implement this function, per its doc-string above.
     # Put a statement in  main  to test this function.
     # REQUIREMENT: You must use a   RANGE  statement to solve this problem.
     # -------------------------------------------------------------------------
@@ -38,6 +39,9 @@ def print_sequence1():
     print('Running print_sequence1:')
     print('--------------------------------------------------')
 
+    total = 0
+    for k in range(21):
+        print(k*10)
 
 def draw_circles1():
     """
@@ -57,8 +61,14 @@ def draw_circles1():
     print('--------------------------------------------------')
     print('Running draw_circles1:  See graphics window')
     print('--------------------------------------------------')
+    radius = 0
+    window = rg.RoseWindow(400,400)
+    rg.Pen('black', 3)
+    for k in range(21):
+        radius = radius + (10*k)
+        rg.Circle(rg.Point(200, 200), radius)
 
-
+    window.close_on_mouse_click()
 def print_sequence2():
     """
     Prints:
